5bc1f7169b8e418753fa02ee48af13bf2f12ba22
krolique <nick@digitalscripter.com>
2017-04-19 19:03:29 -0400

    Removes tokenize_directory and switches to new tokenize func
    -Removed tokenize_directory funcion
    -Switched the module to use the new tokenize util function

--- directories.go
+++ directories.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"path/filepath"
-	"strings"
 )
-
-// tokenizes a directory name into a
-func tokenize_directory(dir_name string) ([]string, error) {
-	// this function defines how each token is determined when used by the
-	// FieldsFunc
-	splitter := func(r rune) bool {
-		switch r {
-		// potential to refine the "case" to include multiple case as
-		// case '<', '>', ':'
-		case '-':
-			return true
-		}
-		return false
-	}
-
-	tokens := strings.FieldsFunc(dir_name, splitter)
-
-	if len(tokens) < 1 {
-		return nil, errors.New("Failed to tokenize the directory name by any of the known delimiters")
-	}
-
-	return tokens, nil
-}
 
 // Scans the folder defined by the `file_path` variable and returns a list
 // of possible album directories
@@ -45,12 +19,7 @@ func scan(file_path string) ([]AlbumDirectory, error) {
 
 	for _, directory := range directories {
 
-		tokens, err := tokenize_directory(directory.Name())
-		if err == nil {
-			for token := range tokens {
-				fmt.Println(token)
-			}
-		}
+		tokens := tokenize(directory.Name(), '-')
 
 		var new_dir AlbumDirectory
 		new_dir.root = file_path
