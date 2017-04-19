23e1d9889f322981c4ddfca7d18b2ffa737f8637
krolique <nick@digitalscripter.com>
2017-04-19 19:17:41 -0400

    Fixes documentation on tokenize func
    Corrected documentation on the tokenize function.

--- utils.go
+++ utils.go
@@ -4,19 +4,20 @@ import (
 	"strings"
 )
 
-// tokenizes a directory name into a
+// tokenizes a given string (value) into string tokens created (split) by
+// the delimiter parameter passed in to the function
 func tokenize(value string, delimiter rune) []string {
 	// this function defines how each token is determined when used by the
 	// FieldsFunc
 	splitter := func(r rune) bool {
 		switch r {
-		// potential to refine the "case" to include multiple case as
-		// case '<', '>', ':'
 		case delimiter:
 			return true
 		}
 		return false
 	}
 
+	// The output of FieldsFunc could be an array with one element or as
+	// many elements as there are tokens.
 	return strings.FieldsFunc(value, splitter)
 }
